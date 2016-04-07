91d7c65356b3473d5076622b6b8d333508312962
Oliver Maurhart <oliver.maurhart@ait.ac.at>
2016-04-07 10:13:23 +0200

    don't enforce gcc for compilation
    Removes GCC specific #if macros. This ought to compile also
    elsewhere with other compilers.

--- bin/tools/qkd-view/json_output.cpp
+++ bin/tools/qkd-view/json_output.cpp
@@ -64,17 +64,15 @@ void json_output::dump_investigation_details(std::ostream & cOut, qkd::utility::
     
     std::time_t cTimestamp = std::chrono::system_clock::to_time_t(cInvestigation.timestamp());
     
-#if GCC_VERSION < 4
+    // using std::put_time would be nice
+    // however, this is only supported by most 
+    // recent gcc compiler version >= 5 
+    // =(
     char sTime[128];
     strftime(sTime, 128, "%F %T", std::localtime(&cTimestamp));    
-#endif    
     
     cOut << "{ \"time\":\"" 
-#if GCC_VERSION >= 5    
-            << std::put_time(std::localtime(&cTimestamp), "%F %T") 
-#else
             << sTime
-#endif
             << "\", \"investigation_time\":"
             << std::chrono::duration_cast<std::chrono::milliseconds>(cInvestigation.duration()).count() 
             << " }";
